c8538c315aca580ab1c42a0672e0f74402314dd7
TelesphoreUwabera <t.uwabera@alustudent.com>
2024-09-02 20:42:01 +0200

    :

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module 7-to_kv
+Contains a function that returns a tuple with a string and the square of a number.
+"""
+
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Returns a tuple with a string and the square of a number.
+
+    Args:
+        k (str): A string.
+        v (Union[int, float]): A number (integer or float).
+
+    Returns:
+        Tuple[str, float]: A tuple where the first element is the string k,
+                           and the second element is the square of v as a float.
+    """
+    return (k, float(v ** 2))
