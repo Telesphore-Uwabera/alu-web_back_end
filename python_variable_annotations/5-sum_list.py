7d85010c2ff07f5a6f158793d2a51f1cb8525b78
TelesphoreUwabera <t.uwabera@alustudent.com>
2024-09-02 20:35:00 +0200

    :

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -6,6 +6,7 @@ Contains a function to sum a list of floats.
 
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """
     Sums a list of floats and returns the result.
