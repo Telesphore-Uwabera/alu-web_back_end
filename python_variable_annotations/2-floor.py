f40dfee4f95d2bfb92bface95e36769d86004810
TelesphoreUwabera <t.uwabera@alustudent.com>
2024-09-02 19:52:56 +0200

    :

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module 2-floor
+Contains a function to return the floor of a float.
+"""
+
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float.
+
+    Args:
+        n (float): The float number to floor.
+
+    Returns:
+        int: The floor of the float.
+    """
+    return math.floor(n)
