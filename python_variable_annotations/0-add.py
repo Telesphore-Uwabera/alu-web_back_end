3de7b506011226d9f25d8227a6fe1e94fa3835f0
TelesphoreUwabera <t.uwabera@alustudent.com>
2024-09-02 19:37:29 +0200

    :

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module 0-add
+Contains a function to add two float numbers.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Add two float numbers and return the result.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of the two numbers.
+    """
+    return a + b
+
