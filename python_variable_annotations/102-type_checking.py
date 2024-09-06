26deb486396068638d30aaef4bdd2d0e1b346010
TelesphoreUwabera <t.uwabera@alustudent.com>
2024-09-06 05:22:55 +0200

    Update 102-type_checking.py

--- python_variable_annotations/102-type_checking.py
+++ python_variable_annotations/102-type_checking.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python3
-"""
-Module 102-type_checking
-Contains a function that zooms in on an array.
-"""
+""" Use mypy to validate the following piece of code
+    and apply any necessary changes.
+    def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+        zoomed_in: Tuple = [
+            item for item in lst
+            for i in range(factor)
+        ]
+        return zoomed_in
 
+
+    array = [12, 72, 91]
+
+    zoom_2x = zoom_array(array)
+
+    zoom_3x = zoom_array(array, 3.0) """
 from typing import Tuple, List
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
-    """
-    Zooms in on a tuple by repeating each element `factor` times.
 
-    Args:
-        lst (Tuple[int, ...]): The tuple of integers to zoom in on.
-        factor (int): The number of times to repeat each element. Defaults to 2.
-
-    Returns:
-        List[int]: A new list with elements repeated `factor` times.
-    """
-    zoomed_in: List[int] = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """ Type Checking """
+    zoomed_in: List = [
         item for item in lst
-        for _ in range(factor)
+        for i in range(factor)
     ]
     return zoomed_in
 
-# Test cases
-array = (12, 72, 91)  # Changed to tuple for testing
+
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-# This should raise a TypeError if uncommented, as expected
-# zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
